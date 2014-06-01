cdede1a9bc5c72d05ba868a95cc1759135167cc4
Drew Sears <asears17@my.whitworth.edu>
2014-05-31 20:59:13 -0700

    Changes5
    deleted the weird character, it worked! Everything else however still
    isn’t compiling.

--- MovieExam/MovieExam/main.cpp
+++ MovieExam/MovieExam/main.cpp
@@ -12,7 +12,7 @@
 #include "Theater.h"
 using namespace std;
 int main() {
-    ￼int MOVIECOUNT = 11;
+    int MOVIECOUNT = 11;
     Movie movieListing[] = {
         Movie("Bull Durham", "Comedy", 0), Movie("Ocean's Eleven", "Action", 2),
         Movie("Monte Python's Meaning of Life", "Comedy", 5), Movie("Jaws", "Horror", 7),
@@ -22,31 +22,29 @@ int main() {
         Movie("Usual Suspects", "Action", 23),
     };
 
-
 Theater garland("The Garland", "509-327-2509");
-
     for (int m = 0; m < MOVIECOUNT; m++) {
-    ￼garland.AddMovie(movieListing[m]);
-    ￼}
-int errors = 0;
+￼garland.AddMovie(movieListing[m]);
+    }
+    int errors = 0;
 if (garland.GetMovieForHour(-1) != "") {
     errors++;
     cout << "error: not handling -1 correctly\n";
     }
-    if (garland.GetMovieForHour(25) != "") {
+if (garland.GetMovieForHour(25) != "") {
         errors++;
         cout << "error: not handling 25 correctly\n";
     }
-    if (garland.GetMovieForHour(18) != "Monte Python's Meaning of Life") {
+if (garland.GetMovieForHour(18) != "Monte Python's Meaning of Life") {
         errors++;
         cout << "error: incorrect movie for 18th hour\n";
     }
-    if (garland.GetShowTimeForGenre("Comedy") != 0) {
+if (garland.GetShowTimeForGenre("Comedy") != 0) {
         errors++;
         cout << "error: incorrect Comedy\n";
     }
-    ￼if (errors == 0) cout << "Passed\n";
-    ￼else cout << "Errors";
+￼if (errors == 0) cout << "Passed\n";
+￼else cout << "Errors";
     
 }
 
